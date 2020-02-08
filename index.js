9f8f2da85844447d38127404afb15acc5041c27c
qb1968 <you@example.com>
2020-02-08 09:10:27 -0500

    Walkthrough

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+// require the express npm module, needs to be added to the project using "yarn add" or "npm install"
+// 
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+server.get('/hobbits', (req, res) => {
+  const hobbits = [
+    {
+      id: 1,
+      name: 'Samwise Gamgee',
+    },
+    {
+      id: 2,
+      name: 'Frodo Baggins',
+    },
+  ];
+
+  res.status(200).json(hobbits);
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
